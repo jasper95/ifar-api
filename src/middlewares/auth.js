886b5e30ff142b538ff2c87bc2d89937d74b38ff
Jasper Bernales <bernalesjasper@gmail.com>
2019-07-16 23:05:41 +0800

    disable auth for now

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -36,38 +36,38 @@ function matchRoutes(routes, req) {
 }
 
 export default async function authMiddleware(req, res, next) {
-  const DB = serviceLocator.get('DB')
-  req.authenticated = true
-  const { authorization } = req.headers
-  if (matchRoutes(public_routes, req)) {
-    return next()
-  }
-  const token = authorization && authorization.includes('Bearer')
-    ? authorization.replace('Bearer ', '') : ''
-  if (token) {
-    try {
-      const { id, user_id } = jwt.verify(token, process.env.AUTH_SECRET)
-      const [session, user] = await Promise.all([DB.find('user_session', id), DB.find('system_user', user_id)])
-      if (!session || session.status !== 'Online') {
-        req.auth_error = 'Invalid token'
-        req.authenticated = false
-      } else if (session) {
-        req.user = user
-        req.session = session
-      }
-    } catch (err) {
-      req.authenticated = false
-      req.auth_error = 'Invalid access token'
-    }
-  } else if (matchRoutes(basic_auth_routes, req)) {
-    if (req.username !== process.env.BASIC_USERNAME
-      || req.authorization.basic.password !== process.env.BASIC_PASSWORD) {
-      req.auth_error = 'Invalid credentials'
-      req.authenticated = false
-    }
-  } else {
-    req.auth_error = 'Authentication is required'
-    req.authenticated = false
-  }
+  // const DB = serviceLocator.get('DB')
+  // req.authenticated = true
+  // const { authorization } = req.headers
+  // if (matchRoutes(public_routes, req)) {
+  //   return next()
+  // }
+  // const token = authorization && authorization.includes('Bearer')
+  //   ? authorization.replace('Bearer ', '') : ''
+  // if (token) {
+  //   try {
+  //     const { id, user_id } = jwt.verify(token, process.env.AUTH_SECRET)
+  //     const [session, user] = await Promise.all([DB.find('user_session', id), DB.find('system_user', user_id)])
+  //     if (!session || session.status !== 'Online') {
+  //       req.auth_error = 'Invalid token'
+  //       req.authenticated = false
+  //     } else if (session) {
+  //       req.user = user
+  //       req.session = session
+  //     }
+  //   } catch (err) {
+  //     req.authenticated = false
+  //     req.auth_error = 'Invalid access token'
+  //   }
+  // } else if (matchRoutes(basic_auth_routes, req)) {
+  //   if (req.username !== process.env.BASIC_USERNAME
+  //     || req.authorization.basic.password !== process.env.BASIC_PASSWORD) {
+  //     req.auth_error = 'Invalid credentials'
+  //     req.authenticated = false
+  //   }
+  // } else {
+  //   req.auth_error = 'Authentication is required'
+  //   req.authenticated = false
+  // }
   return next()
 }
