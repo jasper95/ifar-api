448393518362eac219b56601d30a36a46afb55df
Jasper Bernales <bernalesjasper@gmail.com>
2019-08-30 22:28:22 +0800

    sql scripts init

---
+++ src/bootstrap/initializers/05_sql.js
@@ -0,0 +1,37 @@
+import { views, functions } from 'config/sql'
+import {
+  serviceLocator
+} from 'utils'
+import util from 'util'
+
+export default async function initializeViews(self) {
+  const knex = serviceLocator.get('knex')
+  await Promise
+    .mapSeries(views, (e) => {
+      self.log('info', 'Initialize View  [name: %s]', e.name)
+      return knex.raw(getViewQuery(e))
+    })
+    .then(() => self.log('info', 'Views successfully initialized'))
+    .catch(err => self.log('error', 'Error initializing views [Error: %s]', util.inspect(err)))
+
+  await Promise
+    .mapSeries(functions, (e) => {
+      self.log('info', 'Initialize Function  [signature: %s]', e.param_signature)
+      const q = getFunctionQuery(e)
+      return knex.raw(q)
+    })
+    .then(() => self.log('info', 'Functions successfully initialized'))
+    .catch(err => self.log('error', 'Error initializing functions [Error: %s]', util.inspect(err)))
+}
+
+function getViewQuery({ name, query }) {
+  return `create or replace view ${name} as ${query}`
+}
+function getFunctionQuery({ return_signature = 'setof record', query, param_signature }) {
+  return `
+    create or replace function ${param_signature}
+    returns ${return_signature} AS $$
+      ${query}
+    $$ LANGUAGE sql;
+  `
+}
---
+++ src/config/sql.js
@@ -0,0 +1,21 @@
+export const views = [
+  {
+    name: 'risk_dashboard',
+    query: `
+      select r.*, EXISTS(select * from request where risk_id = r.id and type = 'treatment_request') as has_treatment_request, c.name as classification_name
+      from risk r
+      left join classification c
+        on r.classification_id = c.id
+    `
+  }
+]
+export const functions = [
+  {
+    param_signature: 'search_users(search text)',
+    query: `
+      select *
+      from user
+      where 'first_name' ilike ('%' || search || '%') OR 'last_name' ilike ('%' || search || '%')
+    `
+  }
+]
