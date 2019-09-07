47178712457eb03b2ab18e785ec919373a14d0cc
Jasper Bernales <bernalesjasper@gmail.com>
2019-09-07 22:41:34 +0800

    issue again

--- src/app/request/controller.js
+++ src/app/request/controller.js
@@ -28,7 +28,7 @@ export default class RequestController {
           future_treatments: future_treatments.filter(e => e.id !== treatment_id)
         })
       await this.DB.deleteById('request', params)
-    } else if (['EDIT_INHERENT_RISK', 'EDIT_RESIDUAL_RISK'].includes(type)) {
+    } else if (type === 'EDIT_INHERENT_RISK') {
       await this.DB.updateById('risk', risk_details)
     }
     return { success: true }
