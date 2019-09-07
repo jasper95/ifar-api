b8351ef760c7975f45552aa2e8160d449d7f00bc
Jasper Bernales <bernalesjasper@gmail.com>
2019-09-08 03:52:12 +0800

    revert

--- src/app/auth/controller.js
+++ src/app/auth/controller.js
@@ -105,7 +105,7 @@ export default class UserController {
     }
   }
 
-  async login({ params }, res) {
+  async login({ params }) {
     const { email, password } = params
     const [user] = await this.DB.filter('user', { email })
     if (!user) {
@@ -121,7 +121,6 @@ export default class UserController {
       throw { success: false, message: 'Incorrect Password' }
     }
     const token = await this.Model.auth.authenticateUser(user)
-    res.setHeader('Set-Cookie', `token=${token}; HttpOnly`)
     return {
       ...user,
       token
@@ -188,8 +187,7 @@ export default class UserController {
     return { success: true }
   }
 
-  async logout({ session }, res) {
-    res.removeHeader('token');
+  async logout({ session }) {
     return this.DB.deleteById('user_session', { id: session.id })
   }
 }
